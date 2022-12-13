8fe28c67cfbd2cb232e6ed310bf2056ad245f3c1
Dishant Gupta <guptadihant90@gmial.com>
2022-12-13 10:49:26 +0530

    Dart

--- lib/Final vs Const.dart
+++ lib/Final vs Const.dart
@@ -1,3 +1,16 @@
 void main() {
   print('Welcome To Dart');
+
+  //For Final
+  //final String name;
+  //name = 'Dishant';
+
+  //OR
+
+  //final dynamic name;
+  //name = 'Dishant';
+
+  //For CONST
+  // ignore: unused_local_variable
+  const name = "Dishant";
 }
